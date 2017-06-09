cadbdd082ea5b50dc98d8f4f5ee35770175eff4a
Bart Spaans <bart.spaans@gmail.com>
2017-06-09 19:57:16 +0100

    Add credentials input to errand. We golden

--- scripts/upload_to_gcs.sh
+++ scripts/upload_to_gcs.sh
@@ -8,9 +8,13 @@ target="gs://$INPUT_bucket/$tarball"
 echo "Packing $tarball"
 tar -cvzf "$tarball" escape
 
+echo "$INPUT_credentials" > service_account.json
+gcloud auth activate-service-account --key-file service_account.json
+
 echo "Uploading $INPUT_version to ${target}"
 gsutil cp "$tarball" "$target"
 
 echo "Making archive world readable"
 gsutil acl ch -u AllUsers:R "$target"
 
+rm service_account.json
