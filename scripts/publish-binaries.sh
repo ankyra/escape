cc5b3c5ed2c1e4246072618b58670751b7f2d22f
Bart Spaans <bart.spaans@gmail.com>
2017-06-14 17:42:24 +0100

    Compile main index page

--- scripts/publish-binaries.sh
+++ scripts/publish-binaries.sh
@@ -9,7 +9,8 @@ ARCHS="386 amd64"
 echo "$INPUT_credentials" > service_account.json
 gcloud auth activate-service-account --key-file service_account.json
 
-echo "Escape v$INPUT_version\n<ul>" > index.html
+echo "<h2>Escape v$INPUT_version</h2><ul>" > index.html
+echo "<h2>Escape v$INPUT_version</h2><ul>" > main_index.html
 
 for GOOS in $PLATFORMS; do
     for ARCH in $ARCHS; do
@@ -31,6 +32,7 @@ for GOOS in $PLATFORMS; do
         gsutil acl ch -u AllUsers:R "$gcs_target"
         public_url="https://storage.googleapis.com/$INPUT_bucket/escape-client/$INPUT_version/$target"
         echo "<li><a href=\"$public_url\">$target</a></li>" >> index.html
+        echo "<li><a href=\"$public_url\">$target</a></li>" >> main_index.html
     done
 done
 
@@ -41,3 +43,13 @@ gsutil cp "index.html" "$gcs_index"
 gsutil acl ch -u AllUsers:R "$gcs_index"
 echo "Published $gcs_index"
 
+echo "</ul><h3>Older releases</h3><ul class='older-releases'>" >> main_index.html
+gsutil ls 'gs://escape-releases-eu/escape-client/' | while read line ; do \
+    echo $line | sed 's|gs://escape-releases-eu/escape-client/\(.*\)/|<li><a href="https://storage.googleapis.com/'$INPUT_bucket'/escape-client/\1/index.html">\1</a></li>|' | tee --append main_index.html
+done
+echo "</ul>" >> main_index.html
+
+gcs_index="gs://$INPUT_bucket/escape-client/index.html"
+gsutil cp "main_index.html" "$gcs_index"
+gsutil acl ch -u AllUsers:R "$gcs_index"
+echo "Updated $gcs_index"
