fc115812e0a9592311763532028a86923c010f37
Bart Spaans <bart.spaans@gmail.com>
2017-06-14 15:42:27 +0100

    Create downloads.html

--- scripts/publish-binaries.sh
+++ scripts/publish-binaries.sh
@@ -9,6 +9,8 @@ ARCHS="386 amd64"
 echo "$INPUT_credentials" > service_account.json
 gcloud auth activate-service-account --key-file service_account.json
 
+echo "Escape v$INPUT_version\n<ul>" > downloads.html
+
 for GOOS in $PLATFORMS; do
     for ARCH in $ARCHS; do
         target="escape-v$INPUT_version-$GOOS-$ARCH.tgz"
@@ -26,5 +28,12 @@ for GOOS in $PLATFORMS; do
         fi
         gsutil cp "$target" "gs://$INPUT_bucket/$target"
         gsutil acl ch -u AllUsers:R "gs://$INPUT_bucket/$target"
+        public_url="https://storage.googleapis.com/$INPUT_bucket/$target"
+        echo "<li><a href=\"$public_url\">$target</a></li>" > downloads.html
     done
 done
+
+echo "</ul>" > downloads.html
+gsutil cp "downloads.html" "gs://$INPUT_bucket/downloads.html"
+gsutil acl ch -u AllUsers:R "gs://$INPUT_bucket/downloads.html"
+echo "Published https://storage.googleapis.com/$INPUT_bucket/$target"
