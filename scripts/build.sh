7a03903e4fcff66bcde30fff0bc5bbae14f9e270
Bart Spaans <bart.spaans@gmail.com>
2017-07-20 23:09:02 +0100

    Fix typo in scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,4 @@ user_id=$(id -u $(whoami))
 docker run --rm \
     -v "$PWD":/go/src/github.com/ankyra/escape-client \
     -w /go/src/github.com/ankyra/escape-client \
-    golang:1.8 bash -c "(useradd --uid $user_id builder || true) && su builder -p -c \"/usr/local/go/bin/go build -v -o escape\ && mkdir -p docs/cmd && /usr/local/go/bin/go run docs/generate_cmd_docs.go\""
+    golang:1.8 bash -c "(useradd --uid $user_id builder || true) && su builder -p -c \"/usr/local/go/bin/go build -v -o escape && mkdir -p docs/cmd && /usr/local/go/bin/go run docs/generate_cmd_docs.go\""
