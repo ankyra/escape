1a434ae9acfcf4c94aba9dc286418c93cd4234b1
Bart Spaans <bart.spaans@gmail.com>
2017-11-07 12:20:18 +0000

    Fix publish-binaries errand and remove upload-to-gcs.

--- scripts/publish-binaries.sh
+++ scripts/publish-binaries.sh
@@ -2,15 +2,14 @@
 
 set -e -o pipefail
 
-
 PLATFORMS="darwin linux"
 ARCHS="386 amd64"
 
 echo "$INPUT_credentials" > service_account.json
-gcloud auth activate-service-account --key-file service_account.json
 
-echo "<h2>Escape v$INPUT_escape_version</h2><ul>" > index.html
-echo "<h2>Escape v$INPUT_escape_version</h2><ul>" > main_index.html
+if [ "$INPUT_do_upload" = "1" ] ; then 
+    gcloud auth activate-service-account --key-file service_account.json
+fi
 
 for GOOS in $PLATFORMS; do
     for ARCH in $ARCHS; do
@@ -27,29 +26,12 @@ for GOOS in $PLATFORMS; do
         else
             echo "File $target already exists"
         fi
-        gcs_target="gs://$INPUT_bucket/escape/$INPUT_escape_version/$target"
-        gsutil cp "$target" "$gcs_target"
-        gsutil acl ch -u AllUsers:R "$gcs_target"
-        public_url="https://storage.googleapis.com/$INPUT_bucket/escape-client/$INPUT_escape_version/$target"
-        echo "<li><a href=\"$public_url\">$target</a></li>" >> index.html
-        echo "<li><a href=\"$public_url\">$target</a></li>" >> main_index.html
+
+        if [ "$INPUT_do_upload" = "1" ] ; then 
+            gcs_target="gs://$INPUT_bucket/escape/$INPUT_escape_version/$target"
+            gsutil cp "$target" "$gcs_target"
+            gsutil acl ch -u AllUsers:R "$gcs_target"
+            public_url="https://storage.googleapis.com/$INPUT_bucket/escape-client/$INPUT_escape_version/$target"
+        fi
     done
 done
-
-echo "</ul>" >> index.html
-
-gcs_index="gs://$INPUT_bucket/escape-client/$INPUT_escape_version/index.html"
-gsutil cp "index.html" "$gcs_index"
-gsutil acl ch -u AllUsers:R "$gcs_index"
-echo "Published $gcs_index"
-
-echo "</ul><h3>Older releases</h3><ul class='older-releases'>" >> main_index.html
-gsutil ls 'gs://escape-releases-eu/escape-client/' | while read line ; do \
-    echo $line | sed 's|gs://escape-releases-eu/escape-client/\(.*\)/|<li><a href="https://storage.googleapis.com/'$INPUT_bucket'/escape-client/\1/index.html">\1</a></li>|' | tee --append main_index.html
-done
-echo "</ul>" >> main_index.html
-
-gcs_index="gs://$INPUT_bucket/escape-client/index.html"
-gsutil cp "main_index.html" "$gcs_index"
-gsutil acl ch -u AllUsers:R "$gcs_index"
-echo "Updated $gcs_index"
