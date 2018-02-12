fd985e0d451c55e254ac35ab2ee5b0f99241a792
Bart Spaans <bart.spaans@gmail.com>
2018-02-12 15:25:00 +0000

    Change UpdateDeploymentState endpoint

--- model/remote/endpoints.go
+++ model/remote/endpoints.go
@@ -85,5 +85,5 @@ func (s *ServerEndpoints) ProjectEnvironmentState(project, environment string) s
 	return s.ApiServer() + "api/v1/state/" + project + "/environments/" + environment + "/"
 }
 func (s *ServerEndpoints) UpdateDeploymentState(project, environment, deployment string) string {
-	return s.ProjectEnvironmentState(project, environment) + "deployments/" + deployment + "/"
+	return s.ProjectEnvironmentState(project, environment) + "deployments/?deployment=" + url.QueryEscape(deployment)
 }
